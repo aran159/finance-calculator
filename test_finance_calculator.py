d3485b54d7f694203b634b58cc5eca3b89816d84
Ander <ander.aranfdz@gmail.com>
2021-10-17 17:29:31 +0200

    Add test cases

--- test_finance_calculator.py
+++ test_finance_calculator.py
@@ -1,3 +1,4 @@
+import pytest
 from finance_calculator import (
     PensionScheme,
     RegularInvestment
@@ -5,11 +6,17 @@ from finance_calculator import (
 
 
 class TestRegularInvestment:
-    def test_net_outcome(self):
-        test_cases = [
+    @pytest.mark.parametrize(
+        'test_input,expected', 
+        [
             ({'A': 1000, 'R': 0, 't0': 0, 'Ta': 0}, 1000),
             ({'A': 1000, 'R': 0, 't0': 0.2, 'Ta': 0}, 800),
+            ({'A': 1000, 'R': 0.1, 't0': 0, 'Ta': 0}, 1100),
+            ({'A': 1000, 'R': 1.0, 't0': 0.2, 'Ta': 0}, 1600),
+            ({'A': 1000, 'R': 0, 't0': 0, 'Ta': 0.2}, 1000),
+            ({'A': 1000, 'R': 1, 't0': 0, 'Ta': 0.5}, 1500),
+            ({'A': 1000, 'R': 0.1, 't0': 0.9, 'Ta': 0.5}, 105),
         ]
-
-        for test_case_arguments, expected_result in test_cases:
-            assert RegularInvestment(**test_case_arguments).net_outcome == expected_result
+    )
+    def test_net_outcome(self, test_input, expected):
+        assert pytest.approx(RegularInvestment(**test_input).net_outcome) == expected
